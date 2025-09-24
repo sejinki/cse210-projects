9889b691d68ef5b1ac3b4760019477edb98b42b0
Se Jin Ki <sejinki@Ses-MacBook-Pro.local>
2025-09-24 00:25:14 -0600

    prep2 tasks

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,49 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("What is your grade? ");
+        int grade = int.Parse(Console.ReadLine());
+
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else if (grade >= 0)
+        {
+            letter = "F";
+
+        }
+        else
+        {
+            Console.Write("Please enter valid grade percentage. ");
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congrats you passed the course! ");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
+        
+    
     }
 }
