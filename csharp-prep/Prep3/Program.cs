a8abd1e62f23ce2d7af0f5a31c8f97505dcc8f83
Se Jin Ki <sejinki@Ses-MacBook-Pro.local>
2025-09-26 20:37:45 -0600

    prep3-tasks

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,37 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+
+        Console.Write("What is your guess? ");
+        int guessNumber = int.Parse(Console.ReadLine());
+
+        //int magicNumber = 4;
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+
+        while (guessNumber != magicNumber)
+        {
+            if (guessNumber > magicNumber)
+            {
+                Console.WriteLine("lower");
+            }
+            else if (guessNumber < magicNumber)
+            {
+                Console.WriteLine("higher");
+            }
+
+            Console.Write("What is your guess? ");
+            guessNumber = int.Parse(Console.ReadLine());
+        }
+
+
+        Console.WriteLine("you guessed it!");
+        Console.WriteLine();
+
+        //Console.Write("Do you want to play again?");
+
+
     }
 }
