57f96be29e0407dbb72aa86775ada7cb7ebd3e0c
Se Jin Ki <sejinki@Mac.lan>
2025-09-30 21:27:16 -0600

    job-title-descriptions-tasks

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job();
+
+        job1._company = "Goldman Sachs";
+
+        job1._jobTitle = "Software Engineer";
+
+        job1._startYear = 2023;
+
+        job1._endYear = 2030;
+
+
+
+        Job job2 = new Job();
+
+        job2._company = "Microsoft";
+
+        job2._jobTitle = "Cyber Analyst";
+
+        job2._startYear = 2030;
+
+        job2._endYear = 2040;
+
+        job1.DisplayJobDetails();
+
+        job2.DisplayJobDetails();
+
+
     }
 }
+
+
+
+
+//person1 = new Person();
+ //   person1._givenName = "Emma";
+ //   person1._familyName = "Smith";
+ //   person1.ShowWesternName();
+
+ //   person2 = new Person();
+ //   person2._givenName = "Joseph";
+ //   person2._familyName = "Smith";
+ //   person2.ShowWesternName();
