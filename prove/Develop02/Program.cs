164a9e44d95cdc930b316376e8c60f826e0a11d4
Se Jin Ki <sejinki@Ses-MacBook-Pro.local>
2025-10-11 21:07:30 -0700

    journal-display-menu-created

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        int choice = 0;
+
+        while (choice != 5)
+        {
+            Console.WriteLine("Welcome to the Journal Program!");
+            Console.WriteLine("Please select one of the following choices: \n1. Write\n2. Display\n3. Load\n4. Save\n5. Quit ");
+            Console.WriteLine();
+            Console.Write("What would you like to do? ");
+            string journalChoiceMenu = Console.ReadLine();
+            
+            if (int.TryParse(journalChoiceMenu, out choice))
+
+                switch (journalChoiceMenu)
+                {
+                    case "1":
+                        Console.WriteLine("hi");
+                        break;
+                    case "2":
+                        Console.WriteLine("hi");
+                        break;
+                    case "3":
+                        Console.WriteLine("hi");
+                        break;
+                    case "4":
+                        Console.WriteLine("hi");
+                        break;
+                    case "5":
+                        Console.WriteLine("hi");
+                        break;
+                    default:
+                        Console.WriteLine("hi");
+                        break;
+                }
+
+
+        }
     }
 }
