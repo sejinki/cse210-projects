85c58bc5a4bbe47f4e25c8eaadfc47695279a498
Se Jin Ki <sejinki@Mac.lan>
2025-11-09 16:32:42 -0700

    learning4

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,26 @@
 using System;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning04 World!");
+
+        Assignment b1 = new Assignment("Andrew Smith", "Programming with classes");
+
+
+        Console.WriteLine(b1.GetSummary());
+
+        MathAssignment m1 = new MathAssignment("Andrew Smith", "Fractions", "7.3", "8-19");
+
+        Console.WriteLine(m1.GetSummary());
+
+        Console.WriteLine(m1.GetHomeworkList());
+
+        WritingAssigment w1 = new WritingAssigment("Andrew Smith", "European History", "The Causes of World War II");
+        
+        Console.WriteLine(w1.GetSummary());
+        Console.WriteLine(w1.GetWritingInformation());
     }
 }
---
+++ prepare/Learning04/WritingAssignment.cs
@@ -0,0 +1,19 @@
+public class WritingAssigment : Assignment
+{
+    private string _title;
+
+    public WritingAssigment(string studentName, string topic, string title)
+        : base(studentName, topic)
+    {
+        _title = title;
+
+    }
+
+    public string GetWritingInformation()
+    {
+        // Notice that we are calling the getter here because _studentName is private in the base class
+        string studentName = GetStudentName();
+
+        return $"{_title} by {studentName}";
+    }
+}
